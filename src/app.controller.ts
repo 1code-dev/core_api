bc6c03e2fc5ec1d756e49fdc91cc980ee85097ea
Adii <77727680+AdityaMotale@users.noreply.github.com>
2023-08-17 13:01:45 +0530

    completed w/ setup of `exceptionFilter` in `core` module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createHttpError } from './core/helpers.core';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    try {
+      throw new Error('OLD Fuck!');
+    } catch (error) {
+      throw createHttpError({
+        message: 'LOL!',
+        status: 200,
+        stacktrace: 'Another LOL!',
+        hint: 'Avoid LOL!',
+      });
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExceptionFilter } from './core/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalFilters(new ExceptionFilter());
+  await app.listen(process.env.PORT || 1112);
 }
 bootstrap();
