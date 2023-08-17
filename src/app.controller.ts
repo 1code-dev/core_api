02fde3bfab48a69bd4288b66caf8adc96a02838c
Adii <77727680+AdityaMotale@users.noreply.github.com>
2023-08-17 13:24:48 +0530

    added unit tests for current `utils`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { createHttpError } from './core/helpers.core';
+import { redisClient } from './core/db/redis.db';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello() {
-    try {
-      throw new Error('OLD Fuck!');
-    } catch (error) {
-      throw createHttpError({
-        message: 'LOL!',
-        status: 200,
-        stacktrace: 'Another LOL!',
-        hint: 'Avoid LOL!',
-      });
-    }
+  async getHello() {
+    await redisClient.set('user', '1code');
+
+    const user = (await redisClient.get('user')) ?? '';
+
+    return {
+      user,
+    };
   }
 }
