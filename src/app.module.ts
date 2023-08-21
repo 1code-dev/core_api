38eee9d0ae42e2304a760a23898cbdf0faa9c625
Adii <77727680+AdityaMotale@users.noreply.github.com>
2023-08-21 17:00:14 +0530

    created `exercises` module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
+import { ExercisesModule } from './exercises/exercises.module';
 
 @Module({
-  imports: [UsersModule, TracksModule],
+  imports: [UsersModule, TracksModule, ExercisesModule],
   controllers: [AppController],
   providers: [AppService],
 })
