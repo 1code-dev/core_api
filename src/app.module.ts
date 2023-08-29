dfa49349a7ae6d026c3c8f80bb57c49d9b244a38
Adii <77727680+AdityaMotale@users.noreply.github.com>
2023-08-29 23:21:33 +0530

    completed Leaderboard module for `v-0.1` #6

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
 import { ExercisesModule } from './exercises/exercises.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
 
 @Module({
-  imports: [UsersModule, TracksModule, ExercisesModule],
+  imports: [UsersModule, TracksModule, ExercisesModule, LeaderboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
